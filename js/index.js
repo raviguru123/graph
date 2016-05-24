c4801db503d7400b6408be1e45771cb4b48ace3b
raviguru123 <raviguruiitr@gmail.com>
2016-05-24 15:08:40 +0530

    after successfull implement graph using d3 and c3

---
+++ js/index.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded",function(event){
+console.log("hello");
+var chart = c3.generate({
+    bindto: '#chart',
+    data: {
+      columns: [
+        ['data1', 30, 200, 100, 400, 150, 250],
+        ['data2', 50, 20, 10, 40, 15, 25]
+      ],
+      axes: {
+        data2: 'y2' // ADD
+      }
+    },
+    axis: {
+      y2: {
+        show: true // ADD
+      }
+    }
+});
+
+});
+
